0917370302e8164f71c690af21831d907e0c07c3
ChanJLee <1355425625@qq.com>
2018-03-24 22:27:14 +0800

    sync

--- main/main.py
+++ main/main.py
@@ -33,31 +33,36 @@ def encode(img, watermark):
 
     print "start write"
     # 遍历原图中的每一个像素
-    print "start write"
     for x in range(img_w):
         for y in range(img_h):
             pixel = img_bitmap.getpixel((x, y))
             
-            # 读取水印
+            # 读取水印中的每一个像素，取模是为了防止读取位置超过水印的宽高
+            # 比如5 % 2 取模的结果永远不会超过2
             watermark_x = x % watermark_w
             watermark_y = y % watermark_h
+            # 得出要对原图的每个颜色分量写的值（比如黑色就写0，白色就写1）
             bit = 1 if watermark_bitmap.getpixel((watermark_x, watermark_y)) else 0
             new_pixel = []
+
+            # 原图如果是个彩色图，通常返回(R, G, B, A)的元组，但是如果是黑白图就是一个像素
             if isinstance(pixel, Iterable):
+                # (R, G, B, A)的元组每个channel进行写值，即在0位写0或1
                 for channel in pixel:
                     new_pixel.append(set_bit(channel, 0, bit))
             else:
                 new_pixel.append(set_bit(pixel, 0, bit))
+            # 再输出到输出图片里
             img_out_bitmap.putpixel((x, y), tuple(new_pixel))
     return img_out
 
 
 if __name__ == '__main__':
     # test png
-    # img = Image.open("../tiger-1526704_1280.png")
-    # watermark = Image.open("../watermark.png")
-    # out = encode(img, watermark)
-    # out.save("../png_out.png", img.format)
+    img = Image.open("../tiger-1526704_1280.png")
+    watermark = Image.open("../watermark.png")
+    out = encode(img, watermark)
+    out.save("../png_out.png", img.format)
 
     ## test jpg
     # img = Image.open("../F100011059.jpg")
@@ -81,7 +86,7 @@ if __name__ == '__main__':
     # temp.show()
 
     ## test from bytes 2
-    with open('../F100011059.jpg', 'rb') as content_file:
-        content = content_file.read()
-        img = Image.open(StringIO.StringIO(content))
-        img.show()
+    # with open('../F100011059.jpg', 'rb') as content_file:
+    #     content = content_file.read()
+    #     img = Image.open(StringIO.StringIO(content))
+    #     img.show()
